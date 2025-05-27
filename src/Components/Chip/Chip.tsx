be087f83f7014860c65970d12983440107e5edb9
kavitagupta972 <kavitagupta972@gmail.com>
2025-05-27 12:39:27 +0530

    Initial commit - basic folder structure

---
+++ src/Components/Chip/Chip.tsx
@@ -0,0 +1,7 @@
+const Chip = ()=> {
+    return <>
+        <h6>chip</h6>
+    </>
+}
+
+export default Chip;
---
+++ src/Components/ChipsInput/ChipsInput.tsx
@@ -0,0 +1,10 @@
+import Chip from "../Chip/Chip";
+
+const ChipsInput = () => {
+    return <>
+        <h3>Add Chip</h3>
+        <Chip/>
+    </>
+}
+
+export default ChipsInput;
